826307f441c70959e45877874e6615ddfa66c90b
Justin Long <jwlong.angelusdomini@gmail.com>
2011-11-29 00:53:27 -0600

    Finished layout; added background image

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function($) {
  
 	$(".scroll").click(function(event){		
 		event.preventDefault();
-		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 1200);
 	});
 });
